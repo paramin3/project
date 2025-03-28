5144879e4dd39eb264dfb2f0beeddfa545440089
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-29 00:17:14 +0700

    Update SecurityConfig.java

--- src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
+++ src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
-import jakarta.servlet.http.HttpServletResponse;
+
 
 @Configuration
 @EnableWebSecurity
@@ -100,14 +100,12 @@ public class SecurityConfig {
                 .loginPage("/login")
                 .permitAll()
             )
-   .logout(logout -> logout
-    .logoutUrl("/logout")
-    .logoutSuccessHandler((request, response, authentication) -> {
-        response.setStatus(HttpServletResponse.SC_OK);
-    })
-    .invalidateHttpSession(true)
-    .deleteCookies("JSESSIONID")
-);
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+            );
 
         return http.build();
     }
