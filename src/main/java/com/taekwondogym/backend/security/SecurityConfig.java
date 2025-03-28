1329554cfe3f2f10ca176bbffe96ab330b14aa7d
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-29 00:02:53 +0700

    Update SecurityConfig.java

--- src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
+++ src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
