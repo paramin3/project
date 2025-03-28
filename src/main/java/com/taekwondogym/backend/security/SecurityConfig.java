4fc4ceb928366d935bfec271d3f3df13a22fa80b
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-28 16:18:58 +0700

    Update SecurityConfig.java

--- src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
+++ src/main/java/com/taekwondogym/backend/security/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig {
         http
         .cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(List.of("https://adorable-freedom-production.up.railway.app/")); // Frontend origin
+            config.setAllowedOrigins(List.of(
+    "https://adorable-freedom-production.up.railway.app", 
+    "https://project-front-5twq.onrender.com"
+)); // Frontend origin
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowCredentials(true);
