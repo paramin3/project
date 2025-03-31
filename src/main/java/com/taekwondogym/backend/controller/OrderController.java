5f99e63999333ccdc8613c53277e702b182de38b
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-31 10:55:11 +0700

    Update OrderController.java

--- src/main/java/com/taekwondogym/backend/controller/OrderController.java
+++ src/main/java/com/taekwondogym/backend/controller/OrderController.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.time.ZoneId;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -83,7 +83,7 @@ public class OrderController {
             order.setTelephone(telephone);
             order.setDeliveryType(deliveryType);
             order.setTotalAmount(totalAmount);
-            order.setOrderDate(LocalDateTime.now());
+            order.setOrderDate(LocalDateTime.now(ZoneId.of("Asia/Bangkok")));
 
             // Handle shipping address
             if ("shipping".equalsIgnoreCase(deliveryType)) {
