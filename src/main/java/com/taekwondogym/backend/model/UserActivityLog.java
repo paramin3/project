572ae86fcc901b4f0744236a2f00370122d60d2a
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-31 13:10:25 +0700

    Update UserActivityLog.java

--- src/main/java/com/taekwondogym/backend/model/UserActivityLog.java
+++ src/main/java/com/taekwondogym/backend/model/UserActivityLog.java
@@ -21,9 +21,11 @@ public class UserActivityLog {
     private String ipAddress;
 
     @Column(name = "timestamp", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Bangkok")
     private ZonedDateTime timestamp;
-@Column(name = "details", length = 1000)
-private String details;
+    
+    @Column(name = "details", length = 1000)
+    private String details;
 
     // Constructors
     public UserActivityLog() {}
