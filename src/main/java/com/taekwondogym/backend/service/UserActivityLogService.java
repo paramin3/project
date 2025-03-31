3ad5d9edca9063255bed9b8ced2a1a6dbc986879
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-31 13:30:37 +0700

    Update UserActivityLogService.java

--- src/main/java/com/taekwondogym/backend/service/UserActivityLogService.java
+++ src/main/java/com/taekwondogym/backend/service/UserActivityLogService.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
